e2fa5d05e72262278435b95999c0c602158d6f2b
babinvas <babinvas@gmail.com>
2020-10-19 20:09:19 +0300

    Task#1 Added a static field: BuffeRedreader (System.in) and methods: writeMessage(String message), String readString(),  int readInt().

--- src/ConsoleHelper.java
+++ src/ConsoleHelper.java
@@ -1,10 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  JavaRush. Java Multithreading. Уровень 27. Лекция 15 (3008)
 
  * Чат (Chat)
 
- * Вспомогательный класс, для чтения или записи в консоль.
+ * Класс (вспомогательный), для чтения или записи в консоль.
+
+   Вся работа с консолью происходит через этот класс.
  */
 
 public class ConsoleHelper {
+    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+
+    }
+
+    public static String readString() {
+        return "";
+    }
+
+    public static int readInt() {
+        return 0;
+    }
 }

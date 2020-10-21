ae16348d312439a95720fd4dfa7170e920ba2d1b
babinvas <babinvas@gmail.com>
2020-10-21 18:44:17 +0300

    Task#7 Implemented Interface to class Message, created and implemented constructors and getters.

--- src/Message.java
+++ src/Message.java
@@ -1,3 +1,5 @@
+import java.io.Serializable;
+
 /**
  JavaRush. Java Multithreading. Уровень 27. Лекция 15 (3008)
 
@@ -6,5 +8,25 @@
  * Класс, отвечающий за пересылаемые сообщения.
  */
 
-public class Message {
+public class Message implements Serializable {
+    private final MessageType type;
+    private final String data;
+
+    public Message(MessageType type){
+        this.type = type;
+        data = null;
+    }
+
+    public Message(MessageType type, String data){
+        this.type = type;
+        this.data = data;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getData() {
+        return data;
+    }
 }

c02467f1d31e70d475040db0c218cba91e496560
babinvas <babinvas@gmail.com>
2020-10-30 18:48:22 +0300

    #25 Implemented the Client class - Step 2

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,11 @@
 package client;
 
 import Connection;
+import ConsoleHelper;
+import Message;
+import MessageType;
+
+import java.io.IOException;
 
 /**
  Клиент, в начале своей работы, должен запросить у пользователя адрес
@@ -18,6 +23,48 @@ public class Client {
 
 	// Поле-флаг: true, если клиент подсоединен к серверу или в false в противном случае.
 	private volatile boolean clientConnected = false;
+
+	// Метод заправшивает ввод адреса сервера у пользователя.
+	protected String getServerAddress() {
+		return ConsoleHelper.readString();
+	}
+
+	// Метод заправшивает ввод порта сервера у пользователя.
+	protected int getServerPort() {
+		return ConsoleHelper.readInt();
+	}
+
+	// Метод заправшивает ввод имя пользователя у пользователя.
+	protected String getUserName() {
+		return ConsoleHelper.readString();
+	}
+
+	// Метод, в данной реализации клиента, всегда должен возвращать true
+	// (мы всегда отправляем текст введенный в консоль).
+	// Этот метод может быть переопределен, если мы будем писать какой-нибудь другой клиент,
+	// унаследованный от нашего, который не должен отправлять введенный в консоль текст.
+	protected boolean shouldSendTextFromConsole() {
+		return true;
+	}
+
+	// Метод создаёт и возвращать новый объект класса SocketThread.
+	protected SocketThread getSocketThread() {
+		return new SocketThread();
+	}
+
+	// Метод создает новое текстовое сообщение, используя переданный текст
+	// и отправляет его серверу через соединение connection.
+	protected void sendTextMessage(String text) {
+		Message message;
+		try {
+			message = new Message(MessageType.TEXT, text);
+			connection.send(message);
+		}
+		catch (IOException e) {
+			ConsoleHelper.writeMessage("Во время отправки сообщения произошла ошибка.");
+			clientConnected = false;
+		}
+	}
 
 	/**
 	 Клас SocketThread отвечате за поток, устанавливающий сокетное соединение и читающий сообщения сервера.
